1afac095f6fd7c84bcd2b0b8acd7a01db0485846
cidospark-teach <engrcidos@outlook.com>
2022-01-04 11:21:24 +0100

    upload and add photo services added

--- UserRoleMgtApi/UserRoleMgtApi.Services/PhotoService.cs
+++ UserRoleMgtApi/UserRoleMgtApi.Services/PhotoService.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using UserRoleMgtApi.Data.EFCore.Repositories;
 using UserRoleMgtApi.Helpers;
 using UserRoleMgtApi.Models;
+using UserRoleMgtApi.Models.Dtos;
 
 namespace UserRoleMgtApi.Services
 {
@@ -20,6 +23,40 @@ namespace UserRoleMgtApi.Services
             IMapper mapper, UserManager<User> userManager,
             IPhotoRepository photoRepository)
         {
+            var acc = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+            _cloudinary = new Cloudinary(acc);
+            _mapper = mapper;
+            _userMgr = userManager;
+            _photoRepo = photoRepository;
         }
+
+        public async Task<Tuple<bool, PhotoUploadDto>> UploadPhotoAsync(PhotoUploadDto model, string userId)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            using (var stream = model.Photo.OpenReadStream())
+            {
+                var imageUploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(model.Photo.FileName, stream),
+                    Transformation = new Transformation().Width(300).Height(300).Gravity("face").Crop("fill")
+                };
+
+                uploadResult = await _cloudinary.UploadAsync(imageUploadParams);
+            }
+
+            var status = uploadResult.StatusCode.ToString();
+
+            if (status.Equals("OK"))
+            {
+                model.PublicId = uploadResult.PublicId;
+                model.Url = uploadResult.Url.ToString();
+                return new Tuple<bool, PhotoUploadDto>(true, model);
+            }
+
+            return new Tuple<bool, PhotoUploadDto>(false, model);
+
+        }
+
     }
 }
