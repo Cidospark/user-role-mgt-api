2020be9e067d0794ccf298a302df899ad7364c53
Cidospark <cidospark27@gmail.com>
2022-01-07 17:46:20 +0100

    get user photos, get user main photo, set main, unset main, delete ma…

--- UserRoleMgtApi/UserRoleMgtApi.Core/Controllers/PhotoController.cs
+++ UserRoleMgtApi/UserRoleMgtApi.Core/Controllers/PhotoController.cs
@@ -68,36 +68,121 @@ namespace UserRoleMgtApi.Core.Controllers
 
         }
 
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        // api/Photo/get-user-photos?userId=d274fa2f-201a-41f9-8a89-f17c4f07544d
+        [HttpGet("get-user-photos")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetUserPhotos(string userId)
         {
-            return new string[] { "value1", "value2" };
+            var photos = await _photoService.GetUserPhotosAsync(userId);
+            if (photos == null)
+            {
+                ModelState.AddModelError("Not found", "No result found for photos");
+                return NotFound(Util.BuildResponse<ImageUploadResult>(false, "Result is empty", ModelState, null));
+            }
+
+            // map result
+            var listOfUsersToReturn = new List<PhotoToReturnDto>();
+            foreach (var photo in photos)
+            {
+                var photosToReturn = _mapper.Map<PhotoToReturnDto>(photo);
+                listOfUsersToReturn.Add(photosToReturn);
+
+            }
+
+            return Ok(Util.BuildResponse<List<PhotoToReturnDto>>(true, "List of user's photos", null, listOfUsersToReturn));
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // api/Photos/get-user-main-photo?userId=d274fa2f-201a-41f9-8a89-f17c4f07544d
+        [HttpGet("get-user-main-photo")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetUserMainPhoto(string userId)
         {
-            return "value";
+            var photo = await _photoService.GetUserMainPhotoAsync(userId);
+            if (photo == null)
+            {
+                ModelState.AddModelError("Not found", "No result found for main photo");
+                return NotFound(Util.BuildResponse<ImageUploadResult>(false, "Result is empty", ModelState, null));
+            }
+
+            // map result
+            var photosToReturn = _mapper.Map<PhotoToReturnDto>(photo);
+
+
+            return Ok(Util.BuildResponse<PhotoToReturnDto>(true, "User's main photo", null, photosToReturn));
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // api/Photos/set-main-photo/kbumxhsvbyqyebrtagmk?userId=d274fa2f-201a-41f9-8a89-f17c4f07544d
+        [HttpPatch("set-main-photo/{publicId}")]
+        public async Task<IActionResult> SetMainPhoto(string userId, string publicId)
         {
+            //check if user logged is the one making the changes - only works for system using Auth tokens
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserId = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (!userId.Equals(currentUserId))
+            {
+                ModelState.AddModelError("Denied", $"You are not allowed to upload photo for another user");
+                var result2 = Util.BuildResponse<string>(false, "Access denied!", ModelState, "");
+                return BadRequest(result2);
+            }
+
+            var res = await _photoService.SetMainPhotoAsync(userId, publicId);
+            if (!res.Item1)
+            {
+                ModelState.AddModelError("Failed", "Could not set main photo");
+                return BadRequest(Util.BuildResponse<ImageUploadResult>(false, "Set main failed!", ModelState, null));
+            }
+
+            return Ok(Util.BuildResponse<string>(true, "Main photo is set sucessfully!", null, res.Item2));
+
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPatch("unset-main-photo")]
+        public async Task<IActionResult> UnsetMainPhoto(string userId)
         {
+            //check if user logged is the one making the changes - only works for system using Auth tokens
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserId = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (!userId.Equals(currentUserId))
+            {
+                ModelState.AddModelError("Denied", $"You are not allowed to upload photo for another user");
+                var result2 = Util.BuildResponse<string>(false, "Access denied!", ModelState, "");
+                return BadRequest(result2);
+            }
+
+            var res = await _photoService.UnSetMainPhotoAsync(userId);
+            if (!res)
+            {
+                ModelState.AddModelError("Failed", "Could not unset main photo");
+                return BadRequest(Util.BuildResponse<ImageUploadResult>(false, "Unset failed!", ModelState, null));
+            }
+
+            return Ok(Util.BuildResponse<string>(true, "Unset main photo is sucessful!", null, ""));
+
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // api/Photos/delete-photo/publicId=kbumxhsvbyqyebrtagmk?userId=d274fa2f-201a-41f9-8a89-f17c4f07544d
+        [HttpDelete("delete-photo/{publicId}")]
+        public async Task<IActionResult> DeletePhoto(string userId, string publicId)
         {
+            //check if user logged is the one making the changes - only works for system using Auth tokens
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserId = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (!userId.Equals(currentUserId))
+            {
+                ModelState.AddModelError("Denied", $"You are not allowed to upload photo for another user");
+                var result2 = Util.BuildResponse<string>(false, "Access denied!", ModelState, "");
+                return BadRequest(result2);
+            }
+
+            var res = await _photoService.DeletePhotoAsync(publicId);
+            if (!res)
+            {
+                ModelState.AddModelError("Failed", "Could not delete photo");
+                return BadRequest(Util.BuildResponse<ImageUploadResult>(false, "Delete failed!", ModelState, null));
+            }
+
+            return Ok(Util.BuildResponse<string>(true, "Photo deleted sucessful!", null, ""));
+
         }
     }
 }
