f278c31e6659f2e1af919a84703b5ecbc757003e
Cidospark <cidospark27@gmail.com>
2022-01-13 06:44:55 +0100

    added mapping settings to auto mapper

--- UserRoleMgtApi/UserRoleMgtApi.Helpers/AutoMapperProfile.cs
+++ UserRoleMgtApi/UserRoleMgtApi.Helpers/AutoMapperProfile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using AutoMapper;
+using UserRoleMgtApi.Models;
+using UserRoleMgtApi.Models.Dtos;
 
 namespace UserRoleMgtApi.Helpers
 {
@@ -7,6 +9,17 @@ namespace UserRoleMgtApi.Helpers
     {
         public AutoMapperProfile()
         {
+            CreateMap<User, UserToReturnDto>();
+            CreateMap<User, RegisterSuccessDto>()
+                .ForMember(dest => dest.UserId, x => x.MapFrom(x => x.Id))
+                .ForMember(d => d.FullName, x => x.MapFrom(x => $"{x.FirstName} {x.LastName}"));
+
+            CreateMap<RegisterDto, User>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(u => u.Email))
+                .ForMember(dest => dest.Address, x => x.MapFrom(s => new Address { Street = s.Street, State = s.State, Country = s.Country }));
+
+            CreateMap<PhotoUploadDto, Photo>();
+            CreateMap<Photo, PhotoToReturnDto>();
         }
     }
 }
